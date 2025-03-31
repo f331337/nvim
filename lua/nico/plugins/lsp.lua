1f889182dca483b87f82ce6c425c401d0934274c
Gulceac Nicolai <nico@localhost.localdomain>
2025-03-31 19:55:13 +0300

    preselection and confirm on <CR> in lsp-zero

--- lua/nico/plugins/lsp.lua
+++ lua/nico/plugins/lsp.lua
@@ -22,10 +22,15 @@ return {
         sources = {
           { name = 'nvim_lsp' },
         },
+        preselect = 'item',
+        completion = {
+          completeopt = 'menu,menuone,noinsert'
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<CR>'] = cmp.mapping.confirm({ select = false })
         }),
         snippet = {
           expand = function(args)
